bda66387f08486cdae51df9391660d0c1f7b3f8f
lez03 <alessajohar@gmail.com>
2024-01-27 21:31:30 +0800

    update utils

--- lib/util/theme/theme.dart
+++ lib/util/theme/theme.dart
@@ -1,11 +1,11 @@
-import 'package:a_store/util/theme/custom_themes/appbar_theme.dart';
-import 'package:a_store/util/theme/custom_themes/bottom_sheet_theme.dart';
-import 'package:a_store/util/theme/custom_themes/checkbox_theme.dart';
-import 'package:a_store/util/theme/custom_themes/chip_theme.dart';
-import 'package:a_store/util/theme/custom_themes/elevated_button_theme.dart';
-import 'package:a_store/util/theme/custom_themes/outline_button_theme.dart';
-import 'package:a_store/util/theme/custom_themes/text_field_theme.dart';
-import 'package:a_store/util/theme/custom_themes/text_theme.dart';
+import 'package:a_store/util/theme/widget_themes/appbar_theme.dart';
+import 'package:a_store/util/theme/widget_themes/bottom_sheet_theme.dart';
+import 'package:a_store/util/theme/widget_themes/checkbox_theme.dart';
+import 'package:a_store/util/theme/widget_themes/chip_theme.dart';
+import 'package:a_store/util/theme/widget_themes/elevated_button_theme.dart';
+import 'package:a_store/util/theme/widget_themes/outline_button_theme.dart';
+import 'package:a_store/util/theme/widget_themes/text_field_theme.dart';
+import 'package:a_store/util/theme/widget_themes/text_theme.dart';
 import 'package:flutter/material.dart';
 
 class AAppTheme {
@@ -34,7 +34,7 @@ class AAppTheme {
     primaryColor: Colors.blue,
     scaffoldBackgroundColor: Colors.black,
     textTheme: ATextTheme.darkTextTheme,
-     chipTheme: AChipTheme.darkChipThemeData,
+    chipTheme: AChipTheme.darkChipThemeData,
     appBarTheme: AAppBarTheme.darkAppBarTheme,
     checkboxTheme: ACheckboxTheme.darkCheckboxTheme,
     bottomSheetTheme: ABottomSheetTheme.darkBottomSheetTheme,
